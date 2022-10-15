fa3e02a4c90d1d36cbdafc5eb1f9129bc7b735ba
Alejandro Giraldo Duque <alejandro.giraldo@elenas.la>
2022-10-15 12:41:22 -0500

    Feat: upload dependences

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,6 +11,8 @@ var ls = require("local-storage");
 var tools = require("./tools");
 var state = require("./state");
 var localization = require("./localization");
+var rotate = require("leaflet-rotate-map");
+var Compass = require("leaflet-compass");
 require("./polyfill");
 
 var parsedOptions = links.parse(window.location.search.slice(1));
@@ -319,6 +321,10 @@ plan.on("waypointschanged", function (e) {
     }
 });
 
+var comp = new L.Control.Compass({ autoActive: true, showDigit: true });
+
+map.addControl(comp);
+
 L.control
     .locate({
         follow: false,
@@ -372,11 +378,11 @@ const isIOS =
     navigator.userAgent.match(/AppleWebKit/);
 
 function success(pos) {
-    if (!markerRotate) {
-        markerRotate = document.querySelectorAll("leaflet-marker-icon");
-    } else {
-        init();
-    }
+    // if (!markerRotate) {
+    //     markerRotate = document.querySelectorAll("leaflet-marker-icon");
+    // } else {
+    //     init();
+    // }
 
     const crd = pos.coords;
     initLatitude = crd.latitude;
@@ -414,76 +420,3 @@ options = {
 };
 
 id = navigator.geolocation.watchPosition(success, error, options);
-
-function init() {
-    setTimeout(startCompass, 4000);
-    //startBtn.addEventListener("click", startCompass);
-    navigator.geolocation.getCurrentPosition(locationHandler);
-
-    if (!isIOS) {
-        window.addEventListener("deviceorientationabsolute", handler, true);
-    }
-}
-
-function startCompass() {
-    if (isIOS) {
-        DeviceOrientationEvent.requestPermission()
-            .then((response) => {
-                if (response === "granted") {
-                    window.addEventListener("deviceorientation", handler, true);
-                } else {
-                    alert("has to be allowed!");
-                }
-            })
-            .catch(() => alert("not supported"));
-    }
-}
-
-function handler(e) {
-    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    markerRotate[0].style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
-
-    // ±15 degree
-    if (
-        (pointDegree < Math.abs(compass) &&
-            pointDegree + 15 > Math.abs(compass)) ||
-        pointDegree > Math.abs(compass + 15) ||
-        pointDegree < Math.abs(compass)
-    ) {
-        //myPoint.style.opacity = 0;
-    } else if (pointDegree) {
-        //myPoint.style.opacity = 1;
-    }
-}
-
-let pointDegree;
-
-function locationHandler(position) {
-    const { latitude, longitude } = position.coords;
-    pointDegree = calcDegreeToPoint(latitude, longitude);
-
-    if (pointDegree < 0) {
-        pointDegree = pointDegree + 360;
-    }
-}
-
-function calcDegreeToPoint(latitude, longitude) {
-    // Qibla geolocation
-    const point = {
-        lat: 21.422487,
-        lng: 39.826206,
-    };
-
-    const phiK = (point.lat * Math.PI) / 180.0;
-    const lambdaK = (point.lng * Math.PI) / 180.0;
-    const phi = (latitude * Math.PI) / 180.0;
-    const lambda = (longitude * Math.PI) / 180.0;
-    const psi =
-        (180.0 / Math.PI) *
-        Math.atan2(
-            Math.sin(lambdaK - lambda),
-            Math.cos(phi) * Math.tan(phiK) -
-                Math.sin(phi) * Math.cos(lambdaK - lambda)
-        );
-    return Math.round(psi);
-}
