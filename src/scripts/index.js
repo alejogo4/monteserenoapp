c2066008113c607491c8f22ac2e11fb048b12554
Alejandro Giraldo Duque <alejandro.giraldo@elenas.la>
2022-10-26 21:05:54 -0500

    Test rotate

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,414 +20,85 @@ var nameLastSpeech = "";
 var parsedOptions = links.parse(window.location.search.slice(1));
 var mergedOptions = L.extend(leafletOptions.defaultState, parsedOptions);
 var local = localization.get(mergedOptions.language);
+var esri = L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+        id: "mapbox.streets",
+        maxZoom: 24,
+        maxNativeZoom: 18,
+        attribution:
+            "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    }
+);
 
-//Location
-var initLatitude, initLongitude;
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-    document.getElementById("window").innerHTML =
-        "Geolocation is not supported by this browser.";
-}
+var osm = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 24,
+    maxNativeZoom: 19,
+    attribution:
+        '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
 
-function showPosition(position) {
-    initLatitude = position.coords.latitude;
-    initLongitude = position.coords.longitude;
-}
-
-// load only after language was chosen
-
-var mapLayer = leafletOptions.layer;
-var overlay = leafletOptions.overlay;
-var baselayer = ls.get("layer")
-    ? mapLayer[0][ls.get("layer")]
-    : mapLayer[0]["openstreetmap.org"];
-var layers =
-    (ls.get("getOverlay") && [baselayer, overlay["hiking"]]) || baselayer;
 var map = L.map("map", {
-    zoomControl: true,
-    dragging: true,
-    layers: layers,
-    maxZoom: 18,
+    center: [55, 10],
+    zoom: 2,
+    layers: [esri],
+    // worldCopyJump: true,
     rotate: true,
     touchRotate: true,
     rotateControl: {
         closeOnZeroBearing: false,
     },
     bearing: 30,
-}).setView(mergedOptions.center, mergedOptions.zoom);
-
-//map.setBearing(30);
-//map.touchRotate.enable();
-
-// Pass basemap layers
-mapLayer = mapLayer.reduce(function (title, layer) {
-    title[layer.label] = L.tileLayer(layer.tileLayer, {
-        id: layer.label,
-    });
-    return title;
 });
 
-/* Leaflet Controls */
-L.control
-    .layers(mapLayer, overlay, {
-        position: "bottomleft",
-    })
-    .addTo(map);
+// map.setBearing(30);
+// map.touchRotate.enable();
 
-L.control.scale().addTo(map);
-
-/* set about text to attribution control */
-map.attributionControl.setPrefix(local["About"]);
-
-/* Store User preferences */
-// store baselayer changes
-map.on("baselayerchange", function (e) {
-    ls.set("layer", e.name);
-});
-// store overlay add or remove
-map.on("overlayadd", function (e) {
-    ls.set("getOverlay", true);
-});
-map.on("overlayremove", function (e) {
-    ls.set("getOverlay", false);
-});
-
-/* OSRM setup */
-var ReversablePlan = L.Routing.Plan.extend({
-    createGeocoders: function () {
-        var container = L.Routing.Plan.prototype.createGeocoders.call(this);
-        return container;
-    },
-});
-
-/* Setup markers */
-function makeIcon(i, n) {
-    var url = "./images/content/marker-via-icon-2x.png";
-    var markerList = [
-        "./images/content/marker-start-icon-2x.png",
-        "./images/content/marker-end-icon-2x.png",
-    ];
-    if (i === 0) {
-        return L.icon({
-            iconUrl: markerList[0],
-            iconSize: [30, 30],
-            iconAnchor: [10, 10],
-            rotationAngle: 90,
-        });
-    }
-    if (i === n - 1) {
-        return L.icon({
-            iconUrl: markerList[1],
-            iconSize: [20, 56],
-            iconAnchor: [10, 28],
-            rotationAngle: 45,
-        });
-    } else {
-        return L.icon({
-            iconUrl: url,
-            iconSize: [20, 56],
-            iconAnchor: [10, 28],
-            rotationAngle: 45,
-        });
-    }
-}
-
-var plan = new ReversablePlan([], {
-    geocoder: L.Control.Geocoder.nominatim(),
-    routeWhileDragging: true,
-    createMarker: function (i, wp, n) {
-        var options = {
-            draggable: this.draggableWaypoints,
-            icon: makeIcon(i, n),
-        };
-        var marker = L.marker(wp.latLng, options);
-
-        marker.on("click", function () {
-            plan.spliceWaypoints(i, 1);
-        });
-        return marker;
-    },
-    routeDragInterval: options.lrm.routeDragInterval,
-    addWaypoints: true,
-    waypointMode: "snap",
-    position: "topright",
-    useZoomParameter: options.lrm.useZoomParameter,
-    reverseWaypoints: true,
-    dragStyles: options.lrm.dragStyles,
-    geocodersClassName: options.lrm.geocodersClassName,
-    geocoderPlaceholder: function (i, n) {
-        var startend = [
-            local["Start - press enter to drop marker"],
-            local["End - press enter to drop marker"],
-        ];
-        var via = [local["Via point - press enter to drop marker"]];
-        if (i === 0) {
-            return startend[0];
+var layers = L.control
+    .layers(
+        {
+            Empty: L.tileLayer(""),
+            Streets: osm,
+            Satellite: esri,
+        },
+        null,
+        {
+            collapsed: false,
         }
-        if (i === n - 1) {
-            return startend[1];
-        } else {
-            return via;
-        }
-    },
-});
-
-// add marker labels
-var controlOptions = {
-    plan: plan,
-    routeWhileDragging: options.lrm.routeWhileDragging,
-    lineOptions: options.lrm.lineOptions,
-    altLineOptions: options.lrm.altLineOptions,
-    summaryTemplate: options.lrm.summaryTemplate,
-    containerClassName: options.lrm.containerClassName,
-    alternativeClassName: options.lrm.alternativeClassName,
-    stepClassName: options.lrm.stepClassName,
-    language: "es", // we are injecting own translations via osrm-text-instructions
-    showAlternatives: options.lrm.showAlternatives,
-    units: mergedOptions.units,
-    serviceUrl: leafletOptions.services[0].path,
-    useHints: false,
-    services: leafletOptions.services,
-    useZoomParameter: options.lrm.useZoomParameter,
-    routeDragInterval: options.lrm.routeDragInterval,
-    collapsible: options.lrm.collapsible,
-};
-// translate profile names
-for (
-    var profile = 0, len = controlOptions.services.length;
-    profile < len;
-    profile++
-) {
-    controlOptions.services[profile].label =
-        local[controlOptions.services[profile].label];
-}
-
-var router = new L.Routing.OSRMv1(controlOptions);
-router._convertRouteOriginal = router._convertRoute;
-router._convertRoute = function (responseRoute) {
-    // monkey-patch L.Routing.OSRMv1 until it's easier to overwrite with a hook
-    var resp = this._convertRouteOriginal(responseRoute);
-
-    if (resp.instructions && resp.instructions.length) {
-        var i = 0;
-        const value = resp.instructions[0].text;
-        if (resp && value != nameLastSpeech) {
-            nameLastSpeech = value;
-            speechText(value);
-        }
-    }
-
-    return resp;
-};
-var lrmControl = L.Routing.control(
-    Object.assign(controlOptions, {
-        router: router,
-    })
-).addTo(map);
-
-var toolsControl = tools
-    .control(
-        localization.get(mergedOptions.language),
-        localization.getLanguages(),
-        options.tools,
-        [1, 2, 3]
     )
     .addTo(map);
-var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-};
 
-var data = null;
-
-fetch(
-    "https://monteserenoapp-91eed-default-rtdb.firebaseio.com/Points.json",
-    requestOptions
-)
-    .then(function (response) {
-        return response.text();
-    })
-    .then(function (result) {
-        data = Object.values(JSON.parse(result));
-        toolsControl.setPoints(data);
-    })
-    .catch(function (error) {
-        return console.log("error", error);
-    });
-
-var state = state(map, lrmControl, toolsControl, mergedOptions);
-
-plan.on("waypointgeocoded", function (e) {
-    if (
-        plan._waypoints.filter(function (wp) {
-            return !!wp.latLng;
-        }).length < 2
-    ) {
-        map.panTo(e.waypoint.latLng);
-    }
-});
-
-function addWaypoint(waypoint) {
-    var length = lrmControl.getWaypoints().filter(function (pnt) {
-        return pnt.latLng;
-    });
-    length = length.length;
-    if (!length) {
-        //Initial destination
-        lrmControl.spliceWaypoints(0, 1, waypoint);
-    } else {
-        if (length === 1) length = length + 1;
-        //Final destination
-        lrmControl.spliceWaypoints(length - 1, 1, waypoint);
-    }
+var markers = [];
+for (var i in places) {
+    markers.push(
+        L.marker(places[i], {
+            draggable: true,
+        })
+            .bindPopup("<b>" + i + "</b><br>" + loremIpsum)
+            .bindTooltip(
+                "<b>" + i + "</b>",
+                markers.length
+                    ? {}
+                    : {
+                          direction: "right",
+                          permanent: true,
+                      }
+            )
+            .addTo(map)
+    );
 }
 
-// User selected routes
-lrmControl.on("alternateChosen", function (e) {
-    var directions = document.querySelectorAll(".leaflet-routing-alt");
-    if (directions[0].style.display != "none") {
-        directions[0].style.display = "none";
-        directions[1].style.display = "block";
-    } else {
-        directions[0].style.display = "block";
-        directions[1].style.display = "none";
-    }
-});
+var path = L.polyline(route, {
+    renderer: L.canvas(),
+}).addTo(map);
 
-// Route export
-lrmControl.on("routeselected", function (e) {
-    var route = e.route || {};
-    var routeGeoJSON = {
-        type: "Feature",
-        properties: {
-            name: route.name,
-            copyright: {
-                author: "Montesereno",
-                license: "https://www.montesereno.com.co/",
-            },
-            link: {
-                href: window.document.location.href,
-                text: window.document.title,
-            },
-            time: new Date().toISOString(),
-        },
-        geometry: {
-            type: "LineString",
-            coordinates: (route.coordinates || []).map(function (coordinate) {
-                return [coordinate.lng, coordinate.lat];
-            }),
-        },
-    };
-    toolsControl.setRouteGeoJSON(routeGeoJSON);
-});
-plan.on("waypointschanged", function (e) {
-    if (
-        !e.waypoints ||
-        e.waypoints.filter(function (wp) {
-            return !wp.latLng;
-        }).length > 0
-    ) {
-        toolsControl.setRouteGeoJSON(null);
-    }
-});
+var circle = L.circle([53, 4], 111111).addTo(map);
 
-var comp = new L.Control.Compass({ autoActive: true, showDigit: true });
+var polygon = L.polygon([
+    [48, -3],
+    [50, -4],
+    [52, 4],
+]).addTo(map);
 
-map.addControl(comp);
-
-L.control
-    .locate({
-        follow: false,
-        setView: true,
-        remainActive: false,
-        keepCurrentZoomLevel: true,
-        stopFollowingOnDrag: false,
-        onLocationError: function (err) {
-            alert(err.message);
-        },
-        onLocationOutsideMapBounds: function (context) {
-            alert(context.options.strings.outsideMapBoundsMsg);
-        },
-        showPopup: false,
-        locateOptions: {},
-    })
-    .addTo(map);
-
-//Change to points search
-
-var finalDestinyLat = null;
-var finalDestinyLong = null;
-
-var markerRotate = null;
-
-addWaypoint();
-
-function addCustomWaypoint(origin, destination) {
-    lrmControl.spliceWaypoints(0, 1, origin);
-    lrmControl.spliceWaypoints(1, 1, destination);
-}
-
-var url = new URL(window.location.href);
-
-if (!url.searchParams.get("srv")) {
-    localStorage.clear();
-} else {
-    var point = localStorage.getItem("point");
-    point
-        ? (document.getElementsByClassName("share-container")[0].value =
-              JSON.parse(point).lote)
-        : null;
-}
-
-var id;
-var target;
-var options;
-
-const isIOS =
-    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-    navigator.userAgent.match(/AppleWebKit/);
-
-function success(pos) {
-    // if (!markerRotate) {
-    //     markerRotate = document.querySelectorAll("leaflet-marker-icon");
-    // } else {
-    //     init();
-    // }
-
-    const crd = pos.coords;
-    initLatitude = crd.latitude;
-    initLongitude = crd.longitude;
-
-    if (localStorage.getItem("point")) {
-        var point = JSON.parse(localStorage.getItem("point"));
-        if (finalDestinyLat != point.lat) {
-            finalDestinyLat = point.lat;
-            finalDestinyLong = point.lan;
-        }
-    }
-    if (finalDestinyLat && finalDestinyLong) {
-        console.log(finalDestinyLat, finalDestinyLong);
-        addCustomWaypoint(
-            L.latLng(initLatitude, initLongitude),
-            L.latLng(finalDestinyLat, finalDestinyLong)
-        );
-    }
-}
-
-function error(err) {
-    console.error(err.code, err.message);
-}
-
-target = {
-    latitude: 0,
-    longitude: 0,
-};
-
-options = {
-    enableHighAccuracy: true,
-    timeout: 1500,
-    maximumAge: 0,
-};
-
-id = navigator.geolocation.watchPosition(success, error, options);
+// Display some debug info
+L.Rotate.debug(map);
